78ccba68779ec800455adb6e802e4547ab7a7d15
Aliiusuf <ialisuf@mail.ru>
2024-04-25 21:37:25 +0300

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,13 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p>&copy; 2024 Все права защищены</p>
+        <p className={styles.p}>Добро пожаловать на MovieMania</p>
+        <p className={styles.p1}>
+          Мы предлагаем широкий выбор кинокартин различных жанров и направлений
+          - от классики до новинок, от драм до боевиков. На MovieMania вы
+          сможете насладиться просмотром любимых фильмов в высоком качестве в
+          любое время и в любом месте!
+        </p>
       </div>
     </footer>
   );

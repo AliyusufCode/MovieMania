44a6691d7da0a6eeee4f07ca2b21e4ce68d7fc69
Aliiusuf <ialisuf@mail.ru>
2024-05-03 21:28:15 +0300

    fixed error

--- components/ButtonsMovie/Buttons.tsx
+++ components/ButtonsMovie/Buttons.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./buttons.module.scss";
 import { MdBookmarkBorder } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
@@ -27,11 +27,6 @@ const Buttons: React.FC<Props> = ({ infoFavorites, movieId }) => {
       dispatch(addFavorite(movieId));
     }
   };
-  // console.log(favorites);
-  // console.log(favoritesId);
-  // console.log(isFavorite);
-  // console.log(movieId);
-
   return (
     <div className={styles.content}>
       <button className={styles.openMovie}>Смотреть фильм</button>
